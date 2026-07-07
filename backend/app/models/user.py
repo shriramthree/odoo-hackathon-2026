169cf42f01ba394c01e33374732f0ca3be5f9381
shriramthree <your_github_email@example.com>
2026-07-07 17:32:33 +0530

    Setup SQLAlchemy database

---
+++ backend/app/models/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from app.database.base import Base
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    name = Column(String)
+
+    email = Column(
+        String,
+        unique=True
+    )
+
+    password = Column(String)
+
+    role = Column(
+        String,
+        default="user"
+    )
